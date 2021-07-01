9d716af680130b7e5c5a04df7445e77221109932
Elton Silva <eltoncarsilva73@gmail.com>
2021-07-01 15:52:34 -0300

    control ship

---
+++ Entities/Player.cs
@@ -0,0 +1,68 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace space_inveders_clone_3d.entities
+{
+    public class Player
+    {
+        public Model Ship;
+        public Matrix World;
+        public Vector3 Position;
+        public Player()
+        {
+            this.Ship = Game1.Instance.Content.Load<Model>("player");
+            this.Position = new Vector3(0f, -12.0f, 2.0f);
+            this.World = Matrix.CreateTranslation(this.Position);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            this.Input();
+            this.Move(gameTime);
+            this.Animation(gameTime);
+        }
+
+        private bool _cRight = false;
+        private bool _cLeft = false;
+        private void Input()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                _cRight = true;
+            if (Keyboard.GetState().IsKeyUp(Keys.Right))
+                _cRight = false;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                _cLeft = true;
+            if (Keyboard.GetState().IsKeyUp(Keys.Left))
+                _cLeft = false;
+        }
+
+        private float _speed = 200f / 10000;
+        private void Move(GameTime gameTime)
+        {
+            if (_cRight)
+                this.Position.X += _speed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (_cLeft)
+                this.Position.X -= _speed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            this.World = Matrix.CreateTranslation(this.Position);
+        }
+
+        private float _maxRotationX = 0.5f;
+        public float Rotation = 0;
+        private void Animation(GameTime gameTime)
+        {
+            if (this._cRight && Rotation <= _maxRotationX)
+                Rotation += 0.03f;
+
+            if (this._cLeft && Rotation >= -_maxRotationX)
+                Rotation -= 0.03f;
+
+            if (!this._cLeft && !this._cRight && Rotation != 0)
+                Rotation = Rotation > 0 ? Rotation - 0.03f : Rotation + 0.03f;
+
+            this.World = Matrix.CreateRotationY(Rotation) * this.World;
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using space_inveders_clone_3d.entities;
+
+namespace space_inveders_clone_3d
+{
+    public class Game1 : Game
+    {
+        public static Game1 Instance;
+
+        private GraphicsDeviceManager _graphics;
+        public SpriteBatch spriteBatch;
+
+        public Player Player;
+
+        public Game1()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            this.Player = new Player();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (this.Player != null)
+                this.Player.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            Vector3 cameraPosition = new Vector3(0.0f, 0.0f, 50.0f);
+            Vector3 cameraTarget = new Vector3(0.0f, 0.0f, 0.0f);
+
+            float fovAngle = MathHelper.ToRadians(45);
+            float aspectRatio = _graphics.PreferredBackBufferWidth / _graphics.PreferredBackBufferHeight;
+            float near = 0.01f;
+            float far = 100f;
+
+            //Matrix world = Matrix.CreateTranslation(new Vector3(0, 10.0f, 0));
+            Matrix view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Up);
+            Matrix projection = Matrix.CreatePerspectiveFieldOfView(fovAngle, 800f / 480f, near, far);
+
+            if (this.Player != null)
+            {
+                DrawModel(this.Player.Ship, this.Player.World, view, projection);
+            }
+
+            base.Draw(gameTime);
+        }
+
+        private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.World = world;
+                    effect.View = view;
+                    effect.Projection = projection;
+
+                    effect.EnableDefaultLighting();
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
